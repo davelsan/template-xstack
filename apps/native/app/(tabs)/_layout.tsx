3ade5d04fb2accb072a889853507f606339738c5
David Velasco <davvelsan@gmail.com>
2024-06-24 23:22:30 +0200

    [#3] Override RN default theme (#6)

--- apps/native/app/(tabs)/_layout.tsx
+++ apps/native/app/(tabs)/_layout.tsx
@@ -1,17 +1,21 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { useStyles } from 'react-native-unistyles';
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-
-import '@theme/index';
+import React, { type ComponentProps } from 'react';
+import { Platform } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { createStyleSheet, useStyles } from 'react-native-unistyles';
+import type { IconProps } from '@expo/vector-icons/build/createIconSet';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
-  const { theme } = useStyles();
+  const { styles, theme } = useStyles(stylesheet);
+  const os = Platform.OS === 'ios' ? 'ios' : 'android';
 
   return (
     <Tabs
       screenOptions={{
+        tabBarStyle: styles[`tabBar_${os}`],
         tabBarActiveTintColor: theme.colors.accentSolid,
+        tabBarInactiveTintColor: theme.colors.grayText,
         headerShown: false,
       }}
     >
@@ -19,6 +23,8 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
+          tabBarLabelStyle: styles.tabBarLabel,
+          header: TabHeader,
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon
               name={focused ? 'home' : 'home-outline'}
@@ -31,6 +37,8 @@ export default function TabLayout() {
         name="explore"
         options={{
           title: 'Explore',
+          tabBarLabelStyle: styles.tabBarLabel,
+          header: TabHeader,
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon
               name={focused ? 'code-slash' : 'code-slash-outline'}
@@ -42,3 +50,30 @@ export default function TabLayout() {
     </Tabs>
   );
 }
+
+function TabHeader() {
+  return <SafeAreaView edges={['top', 'right', 'left']} />;
+}
+
+function TabBarIcon({
+  style,
+  ...rest
+}: IconProps<ComponentProps<typeof Ionicons>['name']>) {
+  return <Ionicons size={28} style={[{ marginBottom: -3 }, style]} {...rest} />;
+}
+
+const stylesheet = createStyleSheet((theme) => ({
+  tabBar_android: {
+    height: 80,
+    paddingBottom: theme.spacing.md,
+  },
+  tabBar_ios: {
+    height: 90,
+  },
+  tabBarLabel: {
+    ...theme.typography.caption,
+  },
+  themeOverrides: {
+    backgroundColor: theme.colors.grayBase,
+  },
+}));
